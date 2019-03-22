cd3aede046cfd31b8ed00d79f9eb414e10121c80
turovtsov <dturovtsov@marathonbet.ru>
2019-03-22 12:31:45 +0300

    Revert "npm update"
    This reverts commit 217a088

---
+++ src/main-urfin-auto-int.js
@@ -0,0 +1,26 @@
+// @flow
+
+import shellInterval from 'shell-interval';
+
+import {appConst} from './const';
+
+const timeout = 25 * 60; // in seconds, here is 25 minutes;
+
+shellInterval({
+    options: {
+        command: `cross-env TIMEOUT=${timeout} babel-node ./src/main-urfin-auto.js`,
+        time: timeout,
+        reps: appConst.interval.main,
+        eager: true,
+    },
+    onExec(error: Error | null, stdout: string, stderr: string) {
+        if (error) {
+            throw error;
+        }
+
+        console.log('shellInterval - onExec:', stdout);
+    },
+    onFinish() {
+        console.log('The shell command was called several times. Exiting...');
+    },
+});
